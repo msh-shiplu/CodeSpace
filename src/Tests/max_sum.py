cd1f3caa60d1687fb2bbc671dff9663ddce2a23d
vtphan <vphan@memphis.edu>
2018-04-08 21:46:38 -0500

    remove files

--- src/Tests/max_sum.py
+++
@@ -1,6 +0,0 @@
-# 5 2 5
-
-def maxsum(L):
-	# two loops to print out all intervals
-	for b in range(len(L)):
-		#...
--- src/Tests/nonproblem.py
+++
@@ -1,4 +0,0 @@
-# This is not a problem with a header
-L = [1,2,3,4,5,6,7]
-for x in L:
-	print(x)
