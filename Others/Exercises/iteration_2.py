34672d34ee66c72bb4e8c90338a8be0c9a956936
vtphan <vphan@memphis.edu>
2018-04-24 23:51:12 -0500

    .

---
+++ Others/Exercises/iteration_2.py
@@ -0,0 +1,17 @@
+# 5 4 3 iterating a list
+
+#--------------------------------------------------------
+# Input: a list of numbers
+# Output: the sum of all numbers in the list
+#--------------------------------------------------------
+def sum_items(L):
+	# initialize sum
+	?
+
+	# iterate through each item of L and update sume
+	for x in L:
+		?
+
+	# return the cummulative sum
+
+
---
+++ Others/Exercises/iteration_4.py
@@ -0,0 +1,17 @@
+# 5 4 3 iterating a file
+
+#--------------------------------------------------------
+# Input: a file name (a string)
+# Output: a list of lines containing a keyword (string)
+#--------------------------------------------------------
+def find_keyword(filename, keyword):
+	# initialize the list "selected_lines"
+	?
+
+	# open file and iterate through each line, and update "selected_lines"
+	for x in L:
+		?
+
+	# return selected_lines
+
+
