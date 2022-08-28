a5f78c0411590e73b9ca0f05a3576bf9a475d1c0
Shiplu Hawlader <shiplu.cse.du@gmail.com>
2022-08-28 00:50:56 -0700

    Added button to export points in a CSV file.

---
+++ src/teacher_export_data.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func exportPointsHandler(w http.ResponseWriter, r *http.Request, who string, uid int) {
+	role := r.FormValue("role")
+	if role == "student" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	rows, err := Database.Query("select student_id, filename, score from problem P, Score S where P.id = S.problem_id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var studentID, score int
+	var filename string
+	var data = make(map[string]map[int]int)
+	for rows.Next() {
+		rows.Scan(&studentID, &filename, &score)
+		if data[filename] == nil {
+			data[filename] = make(map[int]int)
+		}
+		data[filename][studentID] = score
+	}
+	csvData := make([][]string, 1)
+	var i = 1
+	csvData[0] = make([]string, len(data)+1)
+	for k := range data {
+		csvData[0][i] = k
+		i++
+	}
+	for sid := range Students {
+		dt := make([]string, len(data)+1)
+		dt[0] = getName(sid, "student")
+		i = 1
+		for _, v := range data {
+			s, ok := v[sid]
+			if ok {
+				dt[i] = strconv.Itoa(s)
+			}
+			i++
+		}
+		csvData = append(csvData, dt)
+	}
+	file, err := os.Create("points.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+
+	defer writer.Flush()
+
+	writer.WriteAll(csvData)
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote("points.csv"))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	http.ServeFile(w, r, file.Name())
+}
--- src/templates.go
+++ src/templates.go
@@ -1078,9 +1078,15 @@ input:checked + .slider:before {
 		</a>
 		Peer Tutoring: 
 		<label class="switch">
-			<input id="peer_tutoring_button" type="checkbox">
+			<input id="peer_tutoring_button" disabled=disabled type="checkbox">
 			<span class="slider round"></span>
 		</label>
+		<a id="export-button" class="button is-primary" href="">
+			<span class="icon is-small">
+			<i class="fa-solid fa-plus"></i>
+			</span>
+			<span>Export Points</span>
+		</a>
 		{{end}}
 		<table class="table sortable">
 				<thead>
@@ -1125,6 +1131,53 @@ $(document).ready(function(){
 	// 	$.post("/set_peer_tutor", {turn_on: valInt, uid: {{.UserID}}, role: {{.UserRole}}{{if ne .Password ""}}, password: {{.Password}}{{end}} }, function(data, status){
 	// 	});
 	// });
+	$('#export-button').click(function(){
+
+		$.ajax({
+			type: "POST",
+			url: "/teacher_exports_point",
+			data: {uid: {{.UserID}}, role: {{.UserRole}}{{if ne .Password ""}}, password: {{.Password}}{{end}} },
+			success: function(response, status, xhr) {
+				// check for a filename
+				var filename = "";
+				var disposition = xhr.getResponseHeader('Content-Disposition');
+				if (disposition && disposition.indexOf('attachment') !== -1) {
+					var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+					var matches = filenameRegex.exec(disposition);
+					if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+				}
+
+				var type = xhr.getResponseHeader('Content-Type');
+				var blob = new Blob([response], { type: type });
+
+				if (typeof window.navigator.msSaveBlob !== 'undefined') {
+					// IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+					window.navigator.msSaveBlob(blob, filename);
+				} else {
+					var URL = window.URL || window.webkitURL;
+					var downloadUrl = URL.createObjectURL(blob);
+
+					if (filename) {
+						// use HTML5 a[download] attribute to specify filename
+						var a = document.createElement("a");
+						// safari doesn't support this yet
+						if (typeof a.download === 'undefined') {
+							window.location = downloadUrl;
+						} else {
+							a.href = downloadUrl;
+							a.download = filename;
+							document.body.appendChild(a);
+							a.click();
+						}
+					} else {
+						window.location = downloadUrl;
+					}
+
+					setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+				}
+			}
+		});
+	});
 	
 });
 </script>
